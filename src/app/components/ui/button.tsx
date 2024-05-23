3892183f93c9420b0751e3d929e067d860aa9c9b
Ahsan <ahsanaz461@gmail.com>
2024-05-23 18:04:21 +0700

    feat: Add landing page

---
+++ src/app/components/ui/button.tsx
@@ -0,0 +1,55 @@
+import { ComponentPropsWithoutRef, ReactNode } from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { default as NextLink, LinkProps as NextLinkProps } from "next/link";
+import cn from "@/lib/clsx";
+
+const buttonVariants = cva(
+  "group inline-flex w-fit px-8 py-3 font-medium text-sm tracking-wider disabled:opacity-50 disabled:scale-100 transition-all duration-300 md:text-base",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-white text-zinc-900 hover:bg-red-500 hover:text-zinc-900 disabled:bg-white disabled:text-black",
+      },
+    },
+  }
+);
+
+type ButtonVariantProps = VariantProps<typeof buttonVariants>;
+
+interface ButtonProps
+  extends ComponentPropsWithoutRef<"button">,
+    ButtonVariantProps {
+  children?: ReactNode;
+}
+
+interface LinkProps extends NextLinkProps, ButtonVariantProps {
+  children?: ReactNode;
+  className?: string;
+}
+
+export function Button({
+  children,
+  variant = "default",
+  className,
+  ...props
+}: Readonly<ButtonProps>) {
+  return (
+    <button {...props} className={cn(buttonVariants({ variant }), className)}>
+      {children}
+    </button>
+  );
+}
+
+export function Link({
+  children,
+  variant = "default",
+  className,
+  ...props
+}: Readonly<LinkProps>) {
+  return (
+    <NextLink {...props} className={cn(buttonVariants({ variant }), className)}>
+      {children}
+    </NextLink>
+  );
+}
