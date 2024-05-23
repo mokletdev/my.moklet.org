80d04caa5027aee86d3ae2fd1919c6a38956390b
Ahsan <ahsanaz461@gmail.com>
2024-05-23 21:25:50 +0700

    fix: Tweak header UI
    Co-authored-by: Kusindra Aji Rabbany <work@benspace.xyz>

--- src/app/components/parts/header.tsx
+++ src/app/components/parts/header.tsx
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import { P } from "../ui/text";
+import { Link } from "../ui/button";
 
 export default function Header() {
   return (
     <div className="z-10 w-full items-center justify-between font-mono text-sm lg:flex">
-      <P className="fixed left-0 top-0 flex w-full justify-center border-b bg-gradient-to-b pb-6 pt-8 font-bold backdrop-blur-2xl border-neutral-800 bg-zinc-800/30 from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:bg-zinc-800/30">
-        Moklet Domains
-      </P>
+      <Link
+        href={"/"}
+        className="fixed group left-0 top-0 flex w-full justify-center border-b bg-gradient-to-b pb-6 pt-8 font-bold backdrop-blur-2xl border-neutral-800 bg-zinc-800/30 from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:bg-zinc-800/30"
+      >
+        <P className="group-hover:text-zinc-900">Moklet Domains</P>
+      </Link>
       <div className="fixed bottom-0 left-0 flex h-32 w-full items-end justify-center bg-gradient-to-t from-zinc-900 via-zinc-900 lg:static lg:size-auto lg:bg-none">
         <a
           className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
